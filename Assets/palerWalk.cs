82b399d1b2987b45c23db2e186af806b234447de
Conall Quinn <Quinn-C196@ulster.ac.uk>
2025-10-28 12:59:44 +0000

    cant remider

--- Assets/palerWalk.cs
+++ Assets/palerWalk.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class palerWalk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity  = - 9.8f;
+   
+    Vector3 velocity;
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller .Move(move *speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }

172331fc16c4fc9d9ba821fab90f844bb89503f6
Conall Quinn <Quinn-C196@ulster.ac.uk>
2025-11-10 14:46:13 +0000

    ball

--- Assets/FireBall/FireBall.cs
+++ Assets/FireBall/FireBall.cs
@@ -6,7 +6,8 @@ public class FireBall : MonoBehaviour
    // public float bulletSpeed = 10f;
     //public float shootCooldown = 0.1f;
     public float launchVelocity = 700f;
-    public GameObject Projectl;
+    public GameObject fBall;
+    public GameObject spawnpoint;
    
     void Start()
     {
@@ -17,8 +18,8 @@ public class FireBall : MonoBehaviour
         if(Input.GetButtonDown("Fire1"))
         {
             Debug.Log("Has shot");
-            GameObject FBall = Instantiate(Projectl,transform. position,transform.rotation);
-            FBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity));
+            GameObject FBall = Instantiate(fBall,transform.position,transform.rotation);
+            FBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity));
         }
     }
 }

d4538f918eaa0a07250d23506a166f9c96835045
Conall Quinn <Quinn-C196@ulster.ac.uk>
2025-11-20 19:01:02 +0000

    fireball texture and environment detail

--- Assets/hp_bar.cs
+++ Assets/hp_bar.cs
@@ -3,9 +3,11 @@ using UnityEngine.UI;
 public class hp_bar : MonoBehaviour
 {
    public Slider slider;
+
+    public hp HP;
     public void SetMaxHealth(int health)
     {  slider.value = health;
-       slider.maxValue = health;
+       slider.maxValue = HP. maxHealth;
     }
    
   public void SetHealth(int health)
